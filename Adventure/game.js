70d477ce7482c409e6b3af9b2148403fb3fd5a5b
James <jamesridgeh@gmail.com>
2019-01-22 10:29:46 -0700

    Game Started

---
+++ Adventure/game.js
@@ -0,0 +1,16 @@
+// Single line comment
+/*
+
+Multiline comment
+
+*/
+
+//alert("Warning! System Contaminated!");
+
+//confirm("Are you okay?");
+
+//prompt("What's your name?");
+
+var pc = prompt("What is your name?");
+
+document.write(pc);

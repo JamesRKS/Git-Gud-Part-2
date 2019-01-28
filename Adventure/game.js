f8f155c94ca903299717a3d7000eb419d3df06bf
James <jamesridgeh@gmail.com>
2019-01-28 10:10:29 -0700

    Introduction

--- Adventure/game.js
+++ Adventure/game.js
@@ -10,7 +10,17 @@ Multiline comment
 //confirm("Are you okay?");
 
 //prompt("What's your name?");
+Game();
 
-var pc = prompt("What is your name?");
-
-document.write(pc);
+function Game(){
+    
+    alert("Where is Ichabod?");
+    
+    var playerName = prompt("What is your name?");
+    
+    alert("Hello, User "+playerName+" I am HistoryBot! The interactive interface of all the known history of Imagin, The Thirteenth Plane of Artists! Today, we shall be going through the History of Artist 324, Ichabod, through the eyes of his brother, Sophales, but with his name replace with your name, to add more immersion. You will also be able to get more possible endings outside of what actually happened. Click okay to Start");
+    
+    IchabodsRoom();
+    
+    function IchabodsRoom(){}
+}
